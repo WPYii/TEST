d9decb14693009819ccbcc422ad09e92d4e871f1
WPYii <88721625+WPYii@users.noreply.github.com>
2022-08-24 22:52:27 +0800

    Update main.java

--- main.java
+++ main.java
@@ -2,4 +2,8 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Main!");
     }
+    
+    public static void fea(){
+        System.out.println("E");
+    }
 }
